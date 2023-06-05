887228886b99e330d2f5c81c4ebd8334f5bbcdbe
daniel castañeda <danielcastanedadev@gmail.com>
2023-06-05 12:41:56 -0500

    implementacion post login

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,7 +1,8 @@
 //import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import upcLogo from './../../assets/LogoHorizontal-upc.jpg'
 import './Login.css'
+import { useState } from 'react';
 
 function Login() {
   const iconStyle: React.CSSProperties = {
@@ -13,25 +14,87 @@ function Login() {
     display: 'inline-block'
   };
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      // Llamada a la función de validación
+      const isValidUser = await validateUser(username, password);
+      
+      if (isValidUser) {
+        // Usuario y contraseña válidos
+        console.log('Inicio de sesión exitoso');
+        // Redirigir a la página "/home"
+        history.push('/home');
+      } else {
+        // Usuario o contraseña incorrectos
+        console.log('Inicio de sesión fallido');
+      }
+    } catch (error) {
+      console.error('Error al validar el usuario:', error);
+    }
+  };
+
+  const validateUser = async (username: string, password: string): Promise<boolean> => {
+    try {
+      // Realizar la solicitud HTTP utilizando fetch
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+
+      // Verificar el estado de la respuesta
+      if (response.ok) {
+        // La solicitud se realizó correctamente, obtener el resultado
+        const result = await response.json();
+        
+        // Realizar la lógica de validación basada en el resultado
+        // Ejemplo básico: verificar si el resultado es "true"
+        return result === true;
+      } else {
+        // La solicitud no se realizó correctamente, lanzar un error
+        throw new Error('Error al realizar la solicitud');
+      }
+    } catch (error) {
+      // Manejar errores de la solicitud
+      console.error('Error al validar el usuario:', error);
+      throw error;
+    }
+  };
+
+
   return (
     <>
       <div className=".containerlogin">
         <div className="content">
           <i style={iconStyle} data-visualcompletion="css-img"></i>
-          <form className="content__form">
+          <form className="content__form" onSubmit={handleFormSubmit}>
             <div className="content__inputs">
               <label>
-                <input className="" type="text" />
+                <input className="" type="text"  id="username" value={username} onChange={handleUsernameChange} />
                 <span>username or email</span>
               </label>
               <label>
-                <input className="" type="password" />
+                <input className="" type="password" id="password" value={password} onChange={handlePasswordChange} />
                 <span>Password</span>
               </label>
             </div>
-            <Link to={'/home'}>
-              <button>Log In</button>
-            </Link>
+              <button type="submit">Log In</button>
           </form>
           <div className="content__or-text">
             <span></span>
